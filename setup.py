2a1c646b1cb7aacfb09a268d873a0e7bc661dddc
Daniel Gatis <danielgatis@gmail.com>
2021-02-05 11:54:05 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="rembg",
-    version="1.0.20",
+    version="1.0.21",
     description="Remove image background",
     long_description=long_description,
     long_description_content_type="text/markdown",
