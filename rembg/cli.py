cc8a588c8e4c8c1135ffda7ad12a74daea2e2837
Daniel Gatis <danielgatis@gmail.com>
2023-06-19 16:47:17 -0300

    split deps

--- rembg/cli.py
+++ rembg/cli.py
@@ -13,7 +13,7 @@ def main() -> None:
                 except pkg_resources.DistributionNotFound:
                     print(f"Missing dependency: '{requirement.project_name}'")
                     print(
-                        f"Please, install rembg with the cli feature: pip install rembg[cli]"
+                        "Please, install rembg with the cli feature: pip install rembg[cli]"
                     )
                     exit(1)
 
